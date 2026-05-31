cb434d9638140e43d3a63839bf437b21cefb612b
Gary Bey <beyhy94@gmail.com>
2026-05-31 16:46:16 +0800

    :heavy_plus_sign: Added unique ROS_DOMAIN_ID to connect free_fleet to minimial nav2_bringup single_tb3 simulation.
    Signed-off-by: Gary Bey <beyhy94@gmail.com>

--- scripts/2_run_docker_container.bash
+++ scripts/2_run_docker_container.bash
@@ -10,6 +10,7 @@ docker run --rm \
 	-e RCL_LOG_LEVEL=debug \
 	-e RCUTILS_COLORIZED_OUTPUT=1 \
 	-e RMW_IMPLEMENTATION=rmw_cyclonedds_cpp \
+	-e ROS_DOMAIN_ID=44 \
 	-v ./free_fleet_examples/config/fleet/nav2_tb3_simulation_fleet_config.yaml:$CONFIG_FILE \
     -v ./free_fleet_examples/maps/turtlebot3_world/nav_graphs/0.yaml:$NAV_GRAPH_FILE \
 free_fleet_adapter:jazzy bash -c \
